a6651eb3cc477f40dcc6602ca00e2d7f0afbba0c
Laura Ferreira <laura.losa.ferreira@gmail.com>
2026-05-25 12:04:26 +0100

    Guarda ficheiros locais e ignora pasta venv

---
+++ init_ehrbase.sql
@@ -0,0 +1,5 @@
+-- Script de inicialização da BD do EHRbase
+-- Ativa as extensões necessárias para o EHRbase funcionar
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+CREATE EXTENSION IF NOT EXISTS "ltree";
+CREATE EXTENSION IF NOT EXISTS "pg_trgm";
